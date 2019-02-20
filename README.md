c55d7294ff3ae43becf4a2b1dcd9bedc3c48620a
tks <none@none.none>
2019-02-20 16:38:39 +0900

    fix exp_num

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 ## Experiments for [Quora Insincere Questions Classification](https://www.kaggle.com/c/quora-insincere-questions-classification)
 
 Change data_dir and embed_dir in exp_*.py to your file locations.
-
 See exp_*.sh and log/*.json for experimental details.
-
-
-It took 16-7 hours in one experiment on my PC(i7 4790k, 32M, GTX1070).
-
--- exp_BERT.sh
+++ exp_BERT.sh
@@ -14,6 +14,6 @@ python exp_bert_base.py --exp_name "base_uncased_8" --exp_num 17 --n_folds 5 --f
 python exp_bert_base.py --exp_name "multi_uncased_8" --exp_num 18 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 64 --gradient_accumulation_steps 8 --num_train_epochs 2 --n_models 3 --n_bertlayer 8 --max_seq_length 50 --bert_model "bert-base-multilingual-uncased"
 
 # 12 layers
-python exp_bert_base.py --exp_name "base_uncased_12" --exp_num 17 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 64 --gradient_accumulation_steps 8 --num_train_epochs 2 --n_models 3 --n_bertlayer 12 --max_seq_length 50 --bert_model "bert-base-uncased"
+python exp_bert_base.py --exp_name "base_uncased_12" --exp_num 19 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 64 --gradient_accumulation_steps 8 --num_train_epochs 2 --n_models 3 --n_bertlayer 12 --max_seq_length 50 --bert_model "bert-base-uncased"
 
-python exp_bert_base.py --exp_name "multi_uncased_12" --exp_num 18 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 64 --gradient_accumulation_steps 8 --num_train_epochs 2 --n_models 3 --n_bertlayer 12 --max_seq_length 50 --bert_model "bert-base-multilingual-uncased"
+python exp_bert_base.py --exp_name "multi_uncased_12" --exp_num 20 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 64 --gradient_accumulation_steps 8 --num_train_epochs 2 --n_models 3 --n_bertlayer 12 --max_seq_length 50 --bert_model "bert-base-multilingual-uncased"
