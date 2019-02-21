d8e7ed704f448be14a222462f41d1f2964bc3140
tks <none@none.none>
2019-02-21 23:08:44 +0900

    split bert log

--- exp_BERT.sh
+++ exp_BERT.sh
@@ -9,11 +9,10 @@ python exp_bert_base.py --exp_name "multi_uncased_4" --exp_num 16 --n_folds 5 --
 # 8 layers
 python exp_bert_base.py --exp_name "base_uncased_8" --exp_num 17 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 64 --gradient_accumulation_steps 8 --num_train_epochs 2 --n_models 3 --n_bertlayers 8 --max_seq_length 50 --bert_model "bert-base-uncased"
 
-
-# NOT DONE
 python exp_bert_base.py --exp_name "multi_uncased_8" --exp_num 18 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 64 --gradient_accumulation_steps 8 --num_train_epochs 2 --n_models 3 --n_bertlayers 8 --max_seq_length 50 --bert_model "bert-base-multilingual-uncased"
 
 # 12 layers
 python exp_bert_base.py --exp_name "base_uncased_12" --exp_num 19 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 64 --gradient_accumulation_steps 8 --num_train_epochs 2 --n_models 3 --n_bertlayers 12 --max_seq_length 50 --bert_model "bert-base-uncased"
 
-python exp_bert_base.py --exp_name "multi_uncased_12" --exp_num 20 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 64 --gradient_accumulation_steps 8 --num_train_epochs 2 --n_models 3 --n_bertlayers 12 --max_seq_length 50 --bert_model "bert-base-multilingual-uncased"
+# NOT DONE
+python exp_bert_base.py --exp_name "multi_uncased_12" --exp_num 20 --n_folds 5 --fold 0 --seed_split 4567 --train_batch_size 57 --gradient_accumulation_steps 9 --num_train_epochs 2 --n_models 3 --n_bertlayers 12 --max_seq_length 50 --bert_model "bert-base-multilingual-uncased"
